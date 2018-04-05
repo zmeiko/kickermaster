eadc9537634021525f0dc94ed8d872b316a71b9a
Алексей Кузнецов <kuznetsov0209@gmail.com>
2018-04-06 00:12:49 +0400

    Add env config

--- models/index.js
+++ models/index.js
@@ -4,20 +4,14 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+var sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  { dialect: "mysql" }
+);
 
 fs
   .readdirSync(__dirname)
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const session = require("koa-session");
@@ -12,9 +14,9 @@ const app = new App({
   clients: wsServer.clients
 });
 
-server.keys = ["secret"];
+server.keys = [process.env.SECRET_KEY];
 server.use(cors({ credentials: true }));
-server.use(session({ domain: "localhost" }, server));
+server.use(session({}, server));
 server.use(passport.initialize());
 server.use(passport.session());
 server.use(bodyParser());
