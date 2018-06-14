f3fdd625ddee485a8e96182b709614f8db0843e5
Aleksey Kuznetsov <a.kuznetsov@mercurydevelopment.com>
2018-06-14 10:27:23 +0400

    Remove goals and games from users response

--- app/usersModule.js
+++ app/usersModule.js
@@ -1,16 +1,8 @@
 const db = require("../models");
 
 async function getUsers() {
-  const users = await db.User.findAll({
-    include: [{ model: db.Goal }, { model: db.Game }]
-  });
-  return users.map(model => {
-    const { Games, ...user } = model.toJSON();
-    return {
-      ...user,
-      games: Games.map(game => game.id)
-    };
-  });
+  const users = await db.User.findAll();
+  return users;
 }
 
 module.exports = {
