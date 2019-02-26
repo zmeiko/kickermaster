69a274e9ffd3ec907d00ece1a5add9b6a6154dc7
Aleksey Kuznetsov <kuznetsov0209@gmail.com>
2019-02-26 13:47:22 +0400

    Revert changes in tournamentTeam.js

--- models/tournamentTeam.js
+++ models/tournamentTeam.js
@@ -1,6 +1,13 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  var TournamentTeam = sequelize.define("TournamentTeam", {}, {});
+  var TournamentTeam = sequelize.define(
+    "TournamentTeam",
+    {
+      tournamentId: DataTypes.INTEGER,
+      teamId: DataTypes.INTEGER
+    },
+    {}
+  );
   TournamentTeam.associate = function({ Team, Tournament }) {
     TournamentTeam.belongsTo(Team);
     TournamentTeam.belongsTo(Tournament);
